f29f3df6bca9fafd2f8c051ea87e561f7d67f5bb
Saunak <saunak@zendrive.com>
2022-04-06 12:27:19 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import React from "react"
+import { StyleSheet, Text, View, Button } from "react-native"
+
+import Zendrive from "react-native-zendrive"
 
 export default function App() {
+  const buttonHandler = () => {
+    Zendrive.setup({
+      driverId: "zd-rn-integration",
+      sdkKey: "RZgwNQ4iCjDzdO1pFxTZBijgAL3X98l7",
+    }).then((result) => {
+      console.log(result)
+    })
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Initialise ZD" onPress={buttonHandler} />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+})
---
+++ ios/expozdintegration.swift
@@ -0,0 +1,8 @@
+//
+//  expozdintegration.swift
+//  expozdintegration
+//
+//  Created by Saunak on 06/04/22.
+//
+
+import Foundation
